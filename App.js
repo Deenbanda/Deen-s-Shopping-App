4ef39fda22a776ed8ea4c7322ac0083e42c513c1
Deenbanda <91737767+Deenbanda@users.noreply.github.com>
2021-11-01 23:57:05 +0000

    Added a login page and a signup page

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import login from './component/screen/login';
+import signup from './component/screen/signup';
+import carts from './component/screen/carts';
+
 
 export default function App() {
+  const MainNavigator = createStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex: 1}}>
+      <NavigationContainer>
+        <MainNavigator.Navigator initialRouteName="login" screenOptions={{headerShown: false}}>
+          <MainNavigator.Screen name="login" component={login}/>
+          <MainNavigator.Screen name="signup" component={signup}/>
+          <MainNavigator.Screen name="carts" component={carts}/>
+        </MainNavigator.Navigator>
+      </NavigationContainer>
     </View>
-  );
+      
+  )
 }
 
 const styles = StyleSheet.create({
